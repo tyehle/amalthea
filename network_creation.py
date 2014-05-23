c46e50d387d5e5557dcaaa1ca522d0af6f5cfc5a
Tobin Yehle <tobinyehle@gmail.com>
2014-05-23 08:27:38 -0400

    add methods for sequential graph generation

--- network_creation.py
+++ network_creation.py
@@ -8,6 +8,9 @@ crimes = db.crimes  # get the reference to the collection
 zipcodes = db.zipcodes
 
 
+# TODO: Change this crap to generate nodes based on a data set
+
+
 def time_window_graph(nodes, node_attributes, window):
     # nodes : list<list<crime>>
     # node_attrs : list<map<attribute,value>>
@@ -97,16 +100,21 @@ def time_window_zip_graph(time_window, node_zips, node_limit=None):
 def time_window_city_graph(time_window, city_names, node_limit=None):
     cities = []
     attributes = []
-    for name in city_names:
-        # get all the zipcodes in the given city
-        zs = [z for z in zipcodes.find({'city': name},
-                                       {'city': 1, 'zip': 1, 'longitude': 1, 'latitude': 1})]
+    for name, state in city_names:
+        # get all the zip codes in the given city
+        zs = [z for z in zipcodes.find({'city': name, 'state': state})]
+
+        if len(zs) is 0:
+            # print an warning and ignore this city
+            print('Warning: no zip codes found for \"{0}, {1}\"'.format(name, state))
+            continue
+
         # find the average (lat,lon)
         lat = reduce(lambda s, z: s+float(z['latitude']), zs, 0.0) / len(zs)
         lon = reduce(lambda s, z: s+float(z['longitude']), zs, 0.0) / len(zs)
         zips = [d['zip'] for d in zs]
         cities.append(zips)
-        attributes.append({'latitude': lat, 'longitude': lon, 'name': name})
+        attributes.append({'latitude': lat, 'longitude': lon, 'city': name, 'state': state, 'zips': zips})
 
     return time_window_area_graph(time_window, cities, attributes, node_limit)
 
@@ -145,3 +153,146 @@ def time_window_area_graph(time_window, areas, node_attributes, node_limit=None)
 def within_window(time_a, time_b, window):
     """ True if time a is within window of time b, False otherwise."""
     return time_b - time_a <= window >= time_a - time_b
+
+
+def sequential_crime_graph(zip_limit=None, node_limit=None):
+    """ Returns a graph where each node represents a crime, and each sequential
+        crime by date is connected by an edge.
+    """
+
+    if node_limit is None:
+        node_limit = 0
+
+    # get the list of all crimes within the limits
+    if zip_limit is not None:
+        if type(zip_limit) is str:
+            zip_limit = [zip_limit]
+        cs = crimes.find({'zipcode': {'$in': zip_limit}}, limit=node_limit).sort('date')
+    else:
+        cs = crimes.find(limit=node_limit).sort('date')
+
+    # make a graph
+    g = igraph.Graph()
+
+    # iterate through the graph, linking each node to the previous one
+    last_vertex = None
+    for c in cs:
+        g.add_vertex(**{str(k): v for k, v in c.iteritems()})
+        if last_vertex is not None:
+            g.add_edge(last_vertex, last_vertex+1, weight=1)
+            last_vertex += 1
+        else:
+            last_vertex = 0
+
+    return g
+
+
+def sequential_zip_graph(node_zips, crime_limit=None):
+    """ Generates a graph where each node represents a single zip code. The
+        weights of the edges between nodes are proportional to the number of
+        sequential crimes between the two nodes.
+
+        The crime limit will restrict the number of crimes the algorithm
+        retrieves from the database.
+    """
+
+    if crime_limit is None:
+        crime_limit = 0
+
+    areas = []
+    for z in node_zips:
+        # find all the info in the zipcode database
+        info = zipcodes.find_one({'zip': z})
+        # convert keys to strings
+        info = {str(k): v for k, v in info.iteritems()}
+        areas.append(info)
+
+    # define the function to check if a crime is in an area
+    crime_in_area = lambda c, a: c['zipcode'] == a['zip']
+
+    # get the list of crimes to build the graph with
+    all_crimes = crimes.find({'zipcode': {'$in': node_zips}}, limit=crime_limit).sort('date')
+
+    return sequential_area_graph(areas, crime_in_area, all_crimes)
+
+
+def sequential_city_graph(city_names, crime_limit=None):
+    """ Build a graph in which each node represents a city, and the edge
+        weights are proportional to number of sequential crimes between two
+        cities.
+
+        The crime limit will restrict the number of crimes the algorithm
+        retrieves from the database.
+    """
+
+    if crime_limit is None:
+        crime_limit = 0
+
+    # find all relevant information about each city
+    areas = []
+    all_zips = []
+    for city, state in city_names:
+        # find all zip codes in the city
+        zips = [z for z in zipcodes.find({'city': city, 'state': state})]
+
+        if len(zips) is 0:
+            # print a warning and ignore this city
+            print('Warning: no zip codes found for \"{0}, {1}\"'.format(city, state))
+            continue
+
+        # find the average (lat,lon)
+        lat = reduce(lambda s, z: s+float(z['latitude']), zips, 0.0) / len(zips)
+        lon = reduce(lambda s, z: s+float(z['longitude']), zips, 0.0) / len(zips)
+
+        codes = [d['zip'] for d in zips]
+        all_zips.extend(codes)
+
+        areas.append({'latitude': lat, 'longitude': lon, 'city': city, 'state': state, 'zips': codes})
+
+    crime_in_area = lambda c, a: c['zipcode'] in a['zips']
+    crime_list = crimes.find({'zipcode': {'$in': all_zips}}, limit=crime_limit).sort('date')
+
+    return sequential_area_graph(areas, crime_in_area, crime_list)
+
+
+def sequential_area_graph(areas, crime_in_area, crime_list):
+    """ Generates a graph where each area is a node, and the weight of each
+        node represents the likelihood of there being sequential crimes in
+        those two areas.
+
+        This function assumes the list of crimes is already sorted by date.
+    """
+    # set up the graph with all of the vertices
+    g = igraph.Graph()
+    for area in areas:
+        g.add_vertex(**area)
+
+    last_vertex = None
+    count = 0
+    for c in crime_list:
+        # simplify the graph after every thousand to make adding more edges faster
+        if count % 1000 is 0:
+            g.simplify(combine_edges=sum)
+            print(count)
+        count += 1
+        # link to the previous vertex
+        if last_vertex is not None:
+            # find what area it is in
+            area = get_index_of_crime(areas, crime_in_area, c)
+            g.add_edge(last_vertex, area, weight=1)
+            last_vertex = area
+        else:
+            last_vertex = get_index_of_crime(areas, crime_in_area, c)
+
+    g.simplify(combine_edges=sum)
+    return g
+
+
+def get_index_of_crime(areas, crime_in_area, crime):
+    """ Gets the index of the area the given crime is in.
+    """
+    # search through all the nodes in the graph
+    for index in range(len(areas)):
+        if crime_in_area(crime, areas[index]):
+            return index
+    return None
--- test_networks.py
+++ test_networks.py
@@ -39,20 +39,59 @@ def test_time_city_graph():
     i = 0
     all_crimes = crimes.find()
     while len(cities) < total:
-        zip_data = zipcodes.find({'zip': all_crimes[i]['zipcode']}, limit=1)
+        zip_data = zipcodes.find_one({'zip': all_crimes[i]['zipcode']}, limit=1)
         try:
-            cities |= {str(zip_data[0]['city'])}
+            cities |= {(str(zip_data['city']), str(zip_data['state']))}
         except:
-            print('{0} entries in zip database for {1}'.format(zip_data.count(), all_crimes[i]['zipcode']))
+            print('No entries in zip database for {0}'.format(all_crimes[i]['zipcode']))
         finally:
             i += 1
 
 
     g = time_window_city_graph(window, cities, node_limit=1000)
-    g.vs['label'] = g.vs['name']
+    g.vs['label'] = g.vs['city']
     g.vs['x'] = [float(x) for x in g.vs['longitude']]
     g.vs['y'] = [-float(y) for y in g.vs['latitude']]
     igraph.plot(g, edge_width=[math.log(w) for w in g.es['weight']])
 
 
-test_time_city_graph()
+def test_sequential_crime_graph():
+    g = sequential_crime_graph(zip_limit='33611', node_limit=30)
+    g.vs['label'] = g.vs['date']
+    g.vs['x'] = [float(x) for x in g.vs['longitude']]
+    g.vs['y'] = [-float(y) for y in g.vs['latitude']]
+    igraph.plot(g)
+
+
+def test_sequetial_zip_graph():
+    # find some zip codes
+    total = 10
+    zips = set()
+    i = 0
+    all_crimes = crimes.find()
+    while len(zips) < total:
+        zips |= {str(all_crimes[i]['zipcode'])}
+        i += 1
+    g = sequential_zip_graph(list(zips), crime_limit=3000)
+    igraph.plot(g, edge_width=[math.log(w) for w in g.es['weight']])
+
+# find some city names
+window = timedelta(hours=8)
+total = 60
+cities = set()
+i = 0
+all_crimes = crimes.find()
+while len(cities) < total:
+    zip_data = zipcodes.find_one({'zip': all_crimes[i]['zipcode']}, limit=1)
+    try:
+        cities |= {(str(zip_data['city']), str(zip_data['state']))}
+    except:
+        print('No entries in zip database for {0}'.format(all_crimes[i]['zipcode']))
+    finally:
+        i += 1
+
+g = sequential_city_graph(cities, crime_limit=300000)
+g.vs['label'] = g.vs['city']
+g.vs['x'] = [float(x) for x in g.vs['longitude']]
+g.vs['y'] = [-float(y) for y in g.vs['latitude']]
+igraph.plot(g, edge_width=[math.log(w) for w in g.es['weight']])
